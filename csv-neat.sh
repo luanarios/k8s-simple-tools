dfb70d94bb63afdcce6db5f5a07177aa1de6c74e
Luana Rios <luanarios@ntopus.com.br>
2024-09-09 15:34:38 -0300

    including arguments

--- csv-neat.sh
+++ csv-neat.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-csv_file="teste.csv"
+# This script expects for a CSV file with columns "context, "namespace", "resource_type", "resource_name" comma separated.
 
-awk 'NR > 1 {print}' "$csv_file" | while IFS=, read -r context namespace resource_type resource_name; do
-    YAML=$(kubectl neat get -- -n $namespace $resource_type $resource_name --context $context -o yaml)
-    if [ $? -eq 0 ]; then
-        echo "$YAML" | kubectl neat > $resource_name.yaml
-        echo "--- YAML exportado em: $resource_name.yaml" 
-    else
-        exit 4
-    fi
-done
+if [ $# -eq 0 ];
+then
+    echo "$0: Missing arguments"
+    exit 1
+elif [ $# -gt 1 ];
+then
+    echo "$0: Too many arguments: $@"
+    exit 1
+else
+    awk 'NR > 1 {print}' "$1" | while IFS=, read -r context namespace resource_type resource_name; do
+        YAML=$(kubectl neat get -- -n $namespace $resource_type $resource_name --context $context -o yaml)
+        if [ $? -eq 0 ]; then
+            echo "$YAML" | kubectl neat > $resource_name.yaml
+            echo "--- Exported YAML: $resource_name.yaml" 
+        else
+            exit 4
+        fi
+    done
+fi
