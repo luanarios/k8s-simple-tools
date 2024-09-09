09aa206ef69295115df353e417530f3fbf6094a1
Luana Rios <luanarios@ntopus.com.br>
2024-09-09 15:23:35 -0300

    including csv-neat.sh

---
+++ csv-neat.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+csv_file="teste.csv"
+
+awk 'NR > 1 {print}' "$csv_file" | while IFS=, read -r context namespace resource_type resource_name; do
+    if [[ "$namespace" = "namespace" ]]; then
+        continue
+    fi
+    YAML=$(kubectl neat get -- -n $namespace $resource_type $resource_name --context $context -o yaml)
+    if [ $? -eq 0 ]; then
+        echo "$YAML" | kubectl neat > $resource_name.yaml
+        echo "--- YAML exportado em: $resource_name.yaml" 
+    else
+        exit 4
+    fi
+done
